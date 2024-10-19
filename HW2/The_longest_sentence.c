b867b76d52fedca62d2617e71f69e5aa1bfa205c
natindo <lyskov256@gmail.com>
2024-10-19 20:16:59 +0300

    uncorrect return code

--- HW2/The_longest_sentence.c
+++ HW2/The_longest_sentence.c
@@ -1,48 +1,48 @@
-// #include <stdio.h>
-//
-// enum {
-//     fileNotOpen = -1
-// };
-//
-// int main()
-// {
-//     FILE* file = fopen("text.txt", "r");
-//     if (file == NULL) {
-//         perror("file not opened");
-//         return fileNotOpen;
-//     }
-//     char c = fgetc(file);
-//     int result = 0;
-//     char buff[256] = { 0 };
-//     while (c != EOF) {
-//         int counter = 0, i = 0;
-//         char tempBuff[256] = { 0 };
-//         while (c != '.') {
-//             if (c == ' ' || c == '\n') {
-//                 counter++;
-//             }
-//             tempBuff[i++] = c;
-//             c = fgetc(file);
-//         }
-//         if (counter > result) {
-//             result = counter;
-//             i = 0;
-//             char d = tempBuff[i];
-//             while (d != '\0') {
-//                 buff[i] = tempBuff[i];
-//                 i++;
-//                 d = tempBuff[i];
-//             }
-//         }
-//         c = fgetc(file);
-//     }
-//     // +1 due to space
-//     int i = 1;
-//     c = buff[i];
-//     while (c != '\0') {
-//         printf("%c", c);
-//         c = buff[++i];
-//     }
-//     printf("\n%d", result);
-//     return 0;
-// }
+#include <stdio.h>
+
+enum {
+    fileNotOpen = -1
+};
+
+int main()
+{
+    FILE* file = fopen("text.txt", "r");
+    if (file == NULL) {
+        perror("file not opened");
+        return fileNotOpen;
+    }
+    char c = fgetc(file);
+    int result = 0;
+    char buff[256] = { 0 };
+    while (c != EOF) {
+        int counter = 0, i = 0;
+        char tempBuff[256] = { 0 };
+        while (c != '.') {
+            if (c == ' ' || c == '\n') {
+                counter++;
+            }
+            tempBuff[i++] = c;
+            c = fgetc(file);
+        }
+        if (counter > result) {
+            result = counter;
+            i = 0;
+            char d = tempBuff[i];
+            while (d != '\0') {
+                buff[i] = tempBuff[i];
+                i++;
+                d = tempBuff[i];
+            }
+        }
+        c = fgetc(file);
+    }
+    // +1 due to space
+    int i = 1;
+    c = buff[i];
+    while (c != '\0') {
+        printf("%c", c);
+        c = buff[++i];
+    }
+    printf("\n%d", result);
+    return 0;
+}
