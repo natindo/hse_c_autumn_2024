9d509e7bd1caaf88135141494636bc3082132b1e
natindo <lyskov256@gmail.com>
2024-10-23 12:28:15 +0300

    bad version

--- HW7/Cool_strcmp.c
+++ HW7/Cool_strcmp.c
@@ -1,6 +1,4 @@
 
-#include <stdio.h>
-#include <string.h>
 int cool_strcmp(const char* str1, const char* str2)
 {
     while (*str1 != '\0' && *str2 != '\0') {
@@ -9,16 +7,5 @@ int cool_strcmp(const char* str1, const char* str2)
         str1++;
         str2++;
     }
-    if (*str1 != '\0' || *str2 != '\0')
-        return *str1 ? 1 : -1;
     return 0;
-}
-
-int main()
-{
-    char* str1 = "abc";
-    char* str2 = "abd"; // e
-    int result = cool_strcmp(str1, str2);
-    int result2 = strcmp(str1, str2);
-    printf("my = %d really = %d\n", result, result2);
 }
