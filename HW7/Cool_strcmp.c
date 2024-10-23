01c59a1967347cf8edd3ce25979c6f4ffb3636c1
natindo <lyskov256@gmail.com>
2024-10-23 12:26:08 +0300

    hw4 cmp done

--- HW7/Cool_strcmp.c
+++ HW7/Cool_strcmp.c
@@ -3,22 +3,19 @@
 #include <string.h>
 int cool_strcmp(const char* str1, const char* str2)
 {
-    while (*str1 != '\0' && *str2 != '\0') {
-        if (*str1 != *str2)
-            return *str1 > *str2 ? (int)*str1 - *str2 - 1 : (*str2 - *str1 - 1) * -1;
+    while (*str1 != '\0' && (*str1 == *str2)) {
         str1++;
         str2++;
     }
-    if (*str1 != '\0' || *str2 != '\0')
-        return *str1 ? 1 : -1;
-    return 0;
+    return *str1 - *str2;
 }
 
-int main()
+int main(void)
 {
-    char* str1 = "abc";
-    char* str2 = "abd"; // e
+    char* str1 = "as";
+    char* str2 = "as11"; // e
     int result = cool_strcmp(str1, str2);
     int result2 = strcmp(str1, str2);
     printf("my = %d really = %d\n", result, result2);
+    return 0;
 }
